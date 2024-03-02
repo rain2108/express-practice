e76c2694051d3e7f668aff4fb2241769fdabc65d
rain2108 <jainrajat5343@gmail.com>
2024-03-03 00:37:31 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const router = require("./userRoutes");
+
+const app = express();
+const port = 4000;
+
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(express.json());
+app.use(router);
+
+
+app.get('/', (req, res) => {
+    
+    res.sendFile(path.join(__dirname + "/index.html"));
+
+})
+
+
+
+app.listen(port,() => {
+    console.log(`server is working on port: ${port}`)
+});
